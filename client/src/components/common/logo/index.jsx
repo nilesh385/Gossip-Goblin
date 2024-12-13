c277d7837bd87e7166cb3046f14fbfe859748c0d
Nilesh Kumar <nileshandnilesh120@gmail.com>
2024-12-13 09:55:36 +0530

    first commit

---
+++ client/src/components/common/logo/index.jsx
@@ -0,0 +1,10 @@
+const Logo = () => {
+  return (
+    <div className="flex p-5  justify-start items-center gap-2">
+      <img src="/logo.png" alt="GG" className="w-8 h-8" />
+      <span className="text-3xl font-semibold ">GossipGoblin</span>
+    </div>
+  );
+};
+
+export default Logo;
